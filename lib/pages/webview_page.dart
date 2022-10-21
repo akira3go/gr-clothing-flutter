70f98c0405de9f06ee886f55020f7757cda40f22
masayasu.mori <>
2022-10-21 22:38:37 +0900

    urlの制御

--- lib/pages/webview_page.dart
+++ lib/pages/webview_page.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:gr_clothing_flutter/gen/colors.gen.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// ignore: must_be_immutable
 class WebviewPage extends StatelessWidget {
   WebviewPage({Key? key, required this.initialUrl}) : super(key: key);
 
@@ -37,7 +39,14 @@ class WebviewPage extends StatelessWidget {
               );
               return NavigationDecision.prevent;
             }
-            return NavigationDecision.navigate;
+            if (_isCanNavigate(request.url)) {
+              return NavigationDecision.navigate;
+            }
+            final uri = Uri.parse(request.url);
+            if (await canLaunchUrl(uri)) {
+              launchUrl(uri);
+            }
+            return NavigationDecision.prevent;
           },
           onWebViewCreated: (controller) {
             this.controller = controller;
@@ -88,5 +97,28 @@ class WebviewPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  bool _isCanNavigate(String url) {
+    const gekirock = "://gekirock.com";
+    const skream = "://skream.jp";
+    const tokyo = "://69hf.tokyo";
+    const barRockaholic = "://bar-rockaholic.jp";
+    const liveholic = "://liveholic.jp";
+    const youtube = "://youtube.com";
+    const twitter = "://twitter.com";
+    const facebook = "://facebook.com";
+    const instagram = "://instagram.com";
+    const rockFashion = "://rock-fashion.jugem.jp";
+    const shopGekirock = "://shop.gekirock.com";
+    const mobileTwitter = "://mobile.twitter.com";
+
+    return url.contains(gekirock) || url.contains(skream) ||
+        url.contains(tokyo) ||
+        url.contains(barRockaholic) || url.contains(liveholic) ||
+        url.contains(youtube) || url.contains(twitter) ||
+        url.contains(facebook) || url.contains(instagram) ||
+        url.contains(rockFashion) || url.contains(shopGekirock) ||
+        url.contains(mobileTwitter);
   }
 }
